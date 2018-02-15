33dd4bf7ea12c3a4b1e8976840d3111fe816a009
Etienne Delorieux <etienne.delorieux@gmail.com>
2018-02-15 12:09:38 +0100

    Seed ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ require 'open-uri'
 
 url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 user_serialized = open(url).read
-user = JSON.parse(user_serialized)
+list = JSON.parse(user_serialized)
 
-puts "drinks"
+list["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+end
